36999ad1bb5be6d7cd7021ef6eaf1d8612806c27
badumbl <aia.vadim88@gmail.com>
2021-05-17 22:38:32 +0300

    Test of Tool Tips added

--- src/test/java/objects/widgets/ToolTips.java
+++ src/test/java/objects/widgets/ToolTips.java
@@ -1,4 +1,51 @@
 package objects.widgets;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class ToolTips {
+
+    private WebDriver driver;
+
+    public ToolTips(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public WebElement go() {
+        return driver.findElement(By.xpath("//span[contains(text(),'Tool Tips')]"));
+    }
+
+    public WebElement hoverButton() {
+        return driver.findElement(By.xpath("//button[@id='toolTipButton']"));
+    }
+
+    public WebElement hoverButtonText() {
+        return driver.findElement(By.xpath("//div[@id='buttonToolTip']"));
+    }
+
+    public WebElement hoverField() {
+        return driver.findElement(By.xpath("//input[@id='toolTipTextField']"));
+    }
+
+    public WebElement hoverFieldText() {
+        return driver.findElement(By.xpath("//div[@id='textFieldToolTip']"));
+    }
+
+    public WebElement hoverLinkOne() {
+        return driver.findElement(By.xpath("//a[contains(text(),'Contrary')]"));
+    }
+
+    public WebElement hoverLinkOneText() {
+        return driver.findElement(By.xpath("//div[@id='contraryTexToolTip']"));
+    }
+
+    public WebElement hoverLinkTwo() {
+        return driver.findElement(By.xpath("//a[contains(text(),'1.10.32')]"));
+    }
+
+    public WebElement hoverLinkTwoText() {
+        return driver.findElement(By.xpath("//div[@id='sectionToolTip']"));
+    }
+
 }
--- src/test/java/test/widgets/TestToolTips.java
+++ src/test/java/test/widgets/TestToolTips.java
@@ -1,15 +1,70 @@
 package test.widgets;
 
 import objects.widgets.ToolTips;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 import test.HelperClass;
+
+import java.util.concurrent.TimeUnit;
 
 public class TestToolTips extends HelperClass {
 
     private ToolTips tt;
+    private Actions act;
+    private Wait<WebDriver> wait;
+    private JavascriptExecutor jse;
 
     @BeforeClass
-    public void init(){
+    public void init() {
+        tt = new ToolTips(getDriver());
+        act = new Actions(getDriver());
+        wait = new FluentWait<WebDriver>(getDriver())
+                .withTimeout(30, TimeUnit.SECONDS)
+                .pollingEvery(1, TimeUnit.MILLISECONDS)
+                .ignoring(NoSuchElementException.class);
+        jse = (JavascriptExecutor) getDriver();
+    }
+
+    @Test(priority = 1)
+    public void goTo() {
+        //Scroll is needed, because footer blocks button on smaller monitors
+        jse.executeScript("window.scrollBy(0,250)");
+        tt.go().click();
+        wait.until((WebDriver wd) -> ExpectedConditions.visibilityOfAllElements());
+
+    }
+
+    @Test(priority = 2)
+    public void hoverOverButton() {
+        jse.executeScript("arguments[0].scrollIntoView();", tt.hoverButton());
+        act.moveToElement(tt.hoverButton()).perform();
+        wait.until((WebDriver wd) -> tt.hoverButtonText().isDisplayed());
+        Assert.assertEquals(tt.hoverButtonText().getText(), "You hovered over the Button");
+    }
+
+    @Test(priority = 3)
+    public void hoverOverTextField() {
+        act.moveToElement(tt.hoverField()).perform();
+        wait.until((WebDriver wd) -> tt.hoverFieldText().isDisplayed());
+        Assert.assertEquals(tt.hoverFieldText().getText(), "You hovered over the text field");
+    }
+
+    @Test(priority = 3)
+    public void hoverOverLinks() {
+        act.moveToElement(tt.hoverLinkOne()).perform();
+        wait.until((WebDriver wd) -> tt.hoverLinkOneText().isDisplayed());
+        Assert.assertEquals(tt.hoverLinkOneText().getText(), "You hovered over the Contrary");
+        act.moveToElement(tt.hoverLinkTwo()).perform();
+        wait.until((WebDriver wd) -> tt.hoverLinkTwoText().isDisplayed());
+        Assert.assertEquals(tt.hoverLinkTwoText().getText(), "You hovered over the 1.10.32");
 
     }
 
